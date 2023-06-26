ca77958de6af981e5b9bb7242ef3d735ff2b8350
Himanshu Choudhary <raptordev21@gmail.com>
2023-06-26 12:48:12 +0530

    home page header added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <div>Hi</div>
+    <header className="flex flex-row justify-between items-center mb-4">
+      <h1 className="text-2xl">Todos</h1>
+      <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href='/new'>New</Link>
+    </header>
   )
 }
